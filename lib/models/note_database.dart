cedcd89657d613cc16abbe8fad219eb75e24773d
CovenantJunior <covenanttope@gmail.com>
2024-01-04 14:29:35 +0100

    Initialize Database

--- lib/models/note_database.dart
+++ lib/models/note_database.dart
@@ -1,12 +1,17 @@
 import 'package:isar/isar.dart';
+import 'package:note_app/models/note.dart';
 import 'package:path_provider/path_provider.dart';
 
 class NoteDatabase {
   static late Isar isar;
 
   // Initialize DB
-  static Future<void> Initialize() async {
-    final dir = getApplicationDocumentsDirectory();
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    final isar = Isar.open(
+      [NoteSchema],
+      directory: dir.path
+    );
   }
 
   // Handle CRUD operations
