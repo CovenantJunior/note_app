33061cc84bd9b6c9465a85b3b08ddd28fbfa3400
CovenantJunior <covenanttope@gmail.com>
2024-01-04 14:15:13 +0100

    Model and `dart run build_runner build`

--- lib/layouts/note_page.dart
+++ lib/layouts/note_page.dart
@@ -10,6 +10,6 @@ class NotePage extends StatefulWidget {
 class _NotePageState extends State<NotePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
