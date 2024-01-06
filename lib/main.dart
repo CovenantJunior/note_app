69b87c6b7cca45d1777cb3cb4c206d7eabcf2864
CovenantJunior <covenanttope@gmail.com>
2024-01-06 15:02:15 +0100

    Wrap App in ChangeNotifierProvider()

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/layouts/note_page.dart';
 import 'package:note_app/models/note_database.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NoteDatabase.initialize();
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => NoteDatabase(),
+      child: const MyApp()
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
