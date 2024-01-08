73c0a24390cbc08194a87a41bf8bd2b4bbfffb0b
CovenantJunior <covenanttope@gmail.com>
2024-01-08 09:52:06 +0100

    Theme Toggle

--- lib/layouts/note_settings.dart
+++ lib/layouts/note_settings.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:note_app/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class NoteSettings extends StatefulWidget {
   const NoteSettings({super.key});
@@ -10,8 +13,30 @@ class NoteSettings extends StatefulWidget {
 class _NoteSettingsState extends State<NoteSettings> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+        titleTextStyle: const TextStyle(
+          fontFamily: "Quicksand",
+          fontSize: 20,
+          fontWeight: FontWeight.bold
+        ),
+        centerTitle: true,
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Dark Mode'),
+            CupertinoSwitch(
+              value: Provider.of<ThemeProvider>(context, listen: false).isDarkMode,
+              onChanged: (value) => Provider.of<ThemeProvider>(context, listen: false).toggleTheme
+            )
+          ],
+        ),
+      ),
     );
   }
 }
